fe4954481dcb9c98357d6e73032cdb403387b5e0
ShobhitWK <shobhit@webkorps.com>
2022-07-23 15:00:44 +0530

    added touch ups

--- resume_generator.rb
+++ resume_generator.rb
@@ -1,3 +1,10 @@
+=begin
+
+Created By: Shobhit Jain
+Task : Resume generator
+
+=end
+
 PHONE_NUMBER_REGEX = /^\(?[\d]{3}\)?[\s|-]?[\d]{3}-?[\d]{4}$/
 EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 DOB_REGEX = /\d{4}-(0[1-9]|1[0-2])-/
