d83be4bf4699b22b3e19f6372f511bb89e8324b6
ShobhitWK <shobhit@webkorps.com>
2022-07-23 14:56:20 +0530

    Made a resume generator

---
+++ modules.rb
@@ -0,0 +1,114 @@
+module ResumeGeneration
+    class ResumeGenerator
+        def initialize
+            @resume_values = Hash.new
+            inputs = get_user_inputs()
+            create_resume_file
+        end
+
+        def get_user_inputs
+
+            name_validated = true
+            while name_validated
+                print "Enter your Name: "
+                val = gets.chomp.to_s
+                if !val.nil? && !val.empty?  # if in input is not given it will not add the query in resume
+                    @resume_values["Name"]=val.capitalize()
+                    name_validated = false
+                else
+                    puts "Enter your name to proceed!!!"
+                end
+            end
+
+            inputList = [
+                "Father Name",
+                "Gender [M/F]",
+                "Address",
+                "Languages",
+                "Secondary class %",
+                "Senior secondary class %",
+                "College aggregate score",
+                "Hobbies",
+                "Strengths",
+            ]
+
+            for index in inputList
+                print "Enter your #{index}: "
+                val = gets.chomp.to_s
+                if !val.nil? && !val.empty?  # if input is not given it will not add the query in resume
+                    @resume_values[index]=val.capitalize()
+                end
+            end
+
+            email_validated = true
+            dob_validated=true
+            phone_validated=true
+
+            while email_validated
+                print "Enter your Mail ID: "
+                email = gets.chomp.to_s
+                if email =~ EMAIL_REGEX                              # /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+                    @resume_values["Mail ID"] = email
+                    email_validated = false
+                else
+                    puts "Invalid email entered!"
+                end
+            end
+
+            while dob_validated
+                print "Enter your date of birth in (yyyy-mm-dd): "
+                dob = gets.chomp.to_s
+                if dob =~ DOB_REGEX                                  # /\d{4}-(0[1-9]|1[0-2])-/
+                    @resume_values["Date of Birth"] = dob
+                    dob_validated=false
+                else
+                    puts "Invalid DOB entered use correct format"
+                end
+            end
+
+            while phone_validated
+                print "Enter your contact no. in 9999999999 format: "
+                phone = gets.chomp.to_s
+                if phone =~ PHONE_NUMBER_REGEX                        # /^\(?[\d]{3}\)?[\s|-]?[\d]{3}-?[\d]{4}$/
+                    @resume_values["Contact No."] = phone
+                    phone_validated=false
+                else
+                    puts "Invalid contact no. entered use correct format"
+                end
+            end
+
+            return @resume_values
+        end
+
+        def create_resume_file
+
+            no = rand(0..99999) # to prevent from same file name error
+
+            unless File.exists?("resume_project/Generated_resumes/#{@resume_values["Name"]}_#{no}.txt")
+                @file = File.new("resume_project/Generated_resumes/#{@resume_values["Name"]}_#{no}.txt","w")
+
+                # Adding values to the file
+
+                @file.puts("===========================================Resume=====================================")
+
+                @resume_values.each { |index,entries|
+
+                    str_len = "| #{index}: #{entries}".length                           # for str length
+                    spaces = 85 - str_len                                               # counting how much space needed
+                    generated_string = "| #{index}: #{entries}" + " "*spaces + "|"      # generating new string with spaces
+
+                    @file.puts(generated_string)
+                }
+
+                @file.puts("======================================================================================")
+                puts "======================================================================================"
+
+                puts "Sucessfully created resume file with name #{@resume_values["Name"]}_#{no}.txt in Generated_resumes folder."
+
+            else
+                puts "Sorry, File with same name already exists please try again!"
+
+            end
+        end
+    end
+end
---
+++ resume_generator.rb
@@ -0,0 +1,17 @@
+PHONE_NUMBER_REGEX = /^\(?[\d]{3}\)?[\s|-]?[\d]{3}-?[\d]{4}$/
+EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+DOB_REGEX = /\d{4}-(0[1-9]|1[0-2])-/
+
+require_relative "modules.rb"
+
+include ResumeGeneration
+
+ResumeGenerator.new
+
+BEGIN{
+    puts "===================================Resume Generator================================="
+}
+
+END{
+    puts "===================================================================================="
+}
